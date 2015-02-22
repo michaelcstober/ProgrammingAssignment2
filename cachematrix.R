55718f5e29fa6d11573cb057223f18a314f7c149
Michael Stober <Michaelcstober@gmail.com>
2015-02-22 13:46:17 -0800

    First Version of Prog Assignment 2
    This is before I fixed the variables and added comments

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,33 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+  cachedinvertedmatrix <- NULL
+  set <- function(y) {
+    x <<- y
+    cachedinvertedmatrix<<- NULL
+  }
+  get <- function() x
+  setinvertedmatrix <- function(invertedmatrix) cachedinvertedmatrix <<- invertedmatrix
+  getinvertedmatrix <- function() cachedinvertedmatrix
+  list(set = set, get = get,
+       setinvertedmatrix = setinvertedmatrix,
+       getinvertedmatrix = getinvertedmatrix)
 
 }
-
+## m= cachedinvertedmatrix
+## 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
+  cachedinvertedmatrix <- x$getinvertedmatrix()
+  if(!is.null(cachedinvertedmatrix)) {
+    message("getting chached data")
+    return(cachedinvertedmatrix)
+  }
+  data <- x$get()
+  cachedinvertedmatrix <- solve(data, ...)
+  x$setinvertedmatrix(cachedinvertedmatrix)
+  cachedinvertedmatrix
         ## Return a matrix that is the inverse of 'x'
 }
